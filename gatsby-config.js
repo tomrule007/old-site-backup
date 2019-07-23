3f39b96eb6293401f2df499ebc5dd3ccd9d34502
Thomas Herzog <Tomrule007@gmail.com>
2019-07-23 15:04:17 -0700

    updated gatsby-config pathPrefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -86,20 +86,20 @@ module.exports = {
         // since these files have unique URLs and their contents will never change
         dontCacheBustUrlsMatching: /(\.js$|\.css$|static\/)/,
         runtimeCaching: [{
-            // Use networkFirst
-            urlPattern: /(\.js$|\.css$|static\/)/,
-            handler: `networkFirst`,
-          },
-          {
-            // Add runtime caching of various other page resources
-            urlPattern: /^https?:.*\.(png|jpg|jpeg|webp|svg|gif|tiff|js|woff|woff2|json|css)$/,
-            handler: `staleWhileRevalidate`,
-          },
-          {
-            // Google Fonts CSS (doesn't end in .css so we need to specify it)
-            urlPattern: /^https?:\/\/fonts\.googleapis\.com\/css/,
-            handler: `staleWhileRevalidate`,
-          },
+          // Use networkFirst
+          urlPattern: /(\.js$|\.css$|static\/)/,
+          handler: `networkFirst`,
+        },
+        {
+          // Add runtime caching of various other page resources
+          urlPattern: /^https?:.*\.(png|jpg|jpeg|webp|svg|gif|tiff|js|woff|woff2|json|css)$/,
+          handler: `staleWhileRevalidate`,
+        },
+        {
+          // Google Fonts CSS (doesn't end in .css so we need to specify it)
+          urlPattern: /^https?:\/\/fonts\.googleapis\.com\/css/,
+          handler: `staleWhileRevalidate`,
+        },
         ],
       },
     },
@@ -114,5 +114,5 @@ module.exports = {
       },
     },
   ],
-  pathPrefix: "/img",
+  pathPrefix: "/tomrule007",
 }
